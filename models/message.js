685fc88fa902fb267910708bc39abf865940a13e
Celeste Lewis <cdlewis42@gmail.com>
2020-07-14 21:34:36 -0400

    the schemas were ok the first time, finished models

--- models/message.js
+++ models/message.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema ({
-    messageId: {type: Number, required: true},
     messageText:{type: String, required: true}
-})
-const messageSchema
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
