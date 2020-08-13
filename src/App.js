14178a80a7433d7fadab97b8d447b781f49006ab
Celeste Lewis <cdlewis42@gmail.com>
2020-08-12 20:51:54 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, {Component} from "react";
 import {BrowserRouter as Router, Route, Switch, Redirect, withRouter} from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Registration from "./pages/Registration";
+import Nav from "./components/Nav";
 class App extends Component{
 render(){
     return(
     <div class = "content-wrap">
+    <Nav></Nav>
     <Router>
     <Route exact path = "/" component = {Registration}/>
     </Router>
---
+++ src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Nav = props => (
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>Profile</li>
+            <li>Inbox</li>
+        </ul>
+    </div>
+)
+
+export default Nav;
