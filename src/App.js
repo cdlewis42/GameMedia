55cbc97c13676ba353782ced80aadf2896c06e88
Celeste Lewis <cdlewis42@gmail.com>
2020-08-15 18:31:08 -0400

    added placeholder for profile pic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {BrowserRouter as Router, Route, Switch, Redirect, withRouter} from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import chicken from "./chicken.jpg";
 import Registration from "./pages/Registration";
 import Nav from "./components/Nav";
 class App extends Component{
@@ -8,6 +9,7 @@ render(){
     return(
     <div class = "content-wrap">
     <Nav></Nav>
+    <img class = "profilePhoto" src = {chicken}></img>
     <Router>
     <Route exact path = "/" component = {Registration}/>
     </Router>
