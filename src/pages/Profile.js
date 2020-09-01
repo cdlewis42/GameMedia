d94431b06d80644096d6f324d31e7bfab4eda1bb
Celeste Lewis <cdlewis42@gmail.com>
2020-09-01 13:57:38 -0400

    added area for rendering name/age/bio on profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,9 +2,18 @@ import React from "react";
 import "./css/registration.css";
 
 class Profile extends React.Component{
+    state = {
+        userName: "Chicken",
+        age: "27",
+        bio: "im just a big chicken"
+    }
     render(){
         return (
-        <div class = "profile">Welcome to the Profile</div>
+        <div class = "profile">Welcome to the Profile
+        <div>Name: {this.state.userName} </div>
+        <div>Age: {this.state.age} </div>
+        <div>Bio:{this.state.bio} </div>
+        </div>
         )
     }
 }; 
